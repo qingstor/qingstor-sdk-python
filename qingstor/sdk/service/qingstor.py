e80691659e895df852accf6e090f650d389f95b6
qiaoqiaolaile <2457254782@qq.com>
2018-06-29 10:44:57 +0800

    Add files via upload

--- qingstor/sdk/service/qingstor.py
+++ qingstor/sdk/service/qingstor.py
@@ -27,8 +27,8 @@ from .bucket import Bucket
 from ..request import Request
 from ..unpack import Unpacker
 
-# QingStor provides QingStor Service API (API Version 2016-01-06)
 
+# QingStor provides QingStor Service API (API Version 2016-01-06)
 
 class QingStor(object):
 
@@ -38,43 +38,53 @@ class QingStor(object):
         retries = Retry(
             total=self.config.connection_retries,
             backoff_factor=1,
-            status_forcelist=[500, 502, 503, 504]
-        )
-        self.client.mount(
-            self.config.protocol + "://", HTTPAdapter(max_retries=retries)
-        )
+            status_forcelist=[500, 502, 503, 504])
+        self.client.mount(self.config.protocol+"://", HTTPAdapter(max_retries=retries))
         if hasattr(self.config, "timeout") and self.config.timeout:
-            self.client.send = partial(
-                self.client.send, timeout=self.config.timeout
-            )
-
-    def list_buckets_request(self, location=""):
+            self.client.send = partial(self.client.send, timeout=self.config.timeout)
+    def list_buckets_request(self,location= ""):
         operation = {
             "API": "ListBuckets",
-            "Method": "GET",
-            "URI": "/",
-            "Headers": {
-                "Host": self.config.host,
+            "Method" : "GET",
+            "URI" : "/",
+            "Headers": {"Host": self.config.host,
                 "Location": location,
             },
-            "Params": {},
-            "Elements": {},
-            "Properties": {},
-            "Body": None
+            "Params": {
+            },
+            "Elements": {
+            },"Properties" : {},
+            "Body" : None
         }
         self.list_buckets_validate(operation)
         return Request(self.config, operation)
-
-    def list_buckets(self, location=""):
+    def list_buckets(self,location= ""):
         req = self.list_buckets_request(location=location)
         resp = self.client.send(req.sign())
         return Unpacker(resp)
-
     @staticmethod
     def list_buckets_validate(op):
         pass
 
+
+
     def Bucket(self, bucket_name, zone):
-        properties = {"bucket-name": bucket_name, "zone": zone}
+        properties = {
+            "bucket-name" : bucket_name,
+            "zone" : zone
+        }
         client = self.client
         return Bucket(self.config, properties, client)
+
+
+
+
+
+
+
+
+
+
+
+
+
